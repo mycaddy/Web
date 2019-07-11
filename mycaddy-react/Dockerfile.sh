f8913a91fc7701da45aaec417c7af6841e5922aa
jjailbird <jjailbird@gmail.com>
2019-07-11 12:55:52 +0900

    [react-api] Modify Dockerfile.sh for batch

--- mycaddy-react/Dockerfile.sh
+++ mycaddy-react/Dockerfile.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+yarn 
+yarn build
 
+docker image rm mycaddy-react-app:0.1 
 docker build --rm -f Dockerfile -t mycaddy-react-app:0.1 .
 docker run --rm -d -p 80:80 mycaddy-react-app:0.1 
