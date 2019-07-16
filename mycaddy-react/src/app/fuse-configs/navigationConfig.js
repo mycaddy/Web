a19d11df5aa2120a1a4609f854d57cef575ebe48
unknown <jjailbird@gmail.com>
2019-07-16 20:09:10 +0900

    [mycaddy-react] Clear unused left navi menu

--- mycaddy-react/src/app/fuse-configs/navigationConfig.js
+++ mycaddy-react/src/app/fuse-configs/navigationConfig.js
@@ -64,50 +64,6 @@ const navigationConfig = [
       },      
     ]
   },
-  {
-    'id': 'e-commerce',
-    'title': 'E-Commerce',
-    'type': 'collapse',
-    'icon': 'shopping_cart',
-    'url': '/apps/e-commerce',
-    'children': [
-      {
-        'id': 'e-commerce-products',
-        'title': 'Products',
-        'type': 'item',
-        'url': '/apps/e-commerce/products',
-        'exact': true
-      },
-      {
-        'id': 'e-commerce-product-detail',
-        'title': 'Product Detail',
-        'type': 'item',
-        'url': '/apps/e-commerce/products/1/a-walk-amongst-friends-canvas-print',
-        'exact': true
-      },
-      {
-        'id': 'e-commerce-new-product',
-        'title': 'New Product',
-        'type': 'item',
-        'url': '/apps/e-commerce/products/new',
-        'exact': true
-      },
-      {
-        'id': 'e-commerce-orders',
-        'title': 'Orders',
-        'type': 'item',
-        'url': '/apps/e-commerce/orders',
-        'exact': true
-      },
-      {
-        'id': 'e-commerce-order-detail',
-        'title': 'Order Detail',
-        'type': 'item',
-        'url': '/apps/e-commerce/orders/1',
-        'exact': true
-      }
-    ]
-  },
 ];
 
 export default navigationConfig;
